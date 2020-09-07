98355de27593ebf79ed1f90628753a8abce1cc27
Heriyanto <heriyanto.liu@gmail.com>
2020-09-07 13:51:50 +0700

    fix select

--- src/components/FormCreateCapex.vue
+++ src/components/FormCreateCapex.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <b-overlay :show="overlay" rounded="sm">
-      <form class="m-form" id="m_form_1" v-on:submit.prevent :aria-hidden="overlay ? 'true' : null">
+      <form
+        class="m-form"
+        id="m_form_1"
+        v-on:submit.prevent
+        :aria-hidden="overlay ? 'true' : null"
+      >
         <div class="m-portlet">
           <div class="m-portlet__head">
             <div class="m-portlet__head-caption">
@@ -18,7 +23,11 @@
                     <label>Budget Owner Name</label>
                   </b-col>
                   <b-col sm="8">
-                    <b-form-input size disabled v-model="budgetOwnerInfo.ownerName"></b-form-input>
+                    <b-form-input
+                      size
+                      disabled
+                      v-model="budgetOwnerInfo.ownerName"
+                    ></b-form-input>
                   </b-col>
                 </b-row>
 
@@ -27,7 +36,11 @@
                     <label>Budget Owner Position</label>
                   </b-col>
                   <b-col sm="8">
-                    <b-form-input size disabled v-model="budgetOwnerInfo.position"></b-form-input>
+                    <b-form-input
+                      size
+                      disabled
+                      v-model="budgetOwnerInfo.position"
+                    ></b-form-input>
                   </b-col>
                 </b-row>
                 <b-row class="my-1">
@@ -35,7 +48,11 @@
                     <label>Budget Owner Payroll ID</label>
                   </b-col>
                   <b-col sm="8">
-                    <b-form-input size disabled v-model="budgetOwnerInfo.payrollID"></b-form-input>
+                    <b-form-input
+                      size
+                      disabled
+                      v-model="budgetOwnerInfo.payrollID"
+                    ></b-form-input>
                   </b-col>
                 </b-row>
 
@@ -54,7 +71,9 @@
                           : false
                       "
                     >
-                      <b-form-invalid-feedback id="cost-center-feedback">Please select Cost Center.</b-form-invalid-feedback>
+                      <b-form-invalid-feedback id="cost-center-feedback"
+                        >Please select Cost Center.</b-form-invalid-feedback
+                      >
                     </comp-select>
                   </b-col>
                 </b-row>
@@ -66,7 +85,9 @@
           <div class="m-portlet__head">
             <div class="m-portlet__head-caption">
               <div class="m-portlet__head-title">
-                <h3 class="m-portlet__head-text">Capital Expenditure Information</h3>
+                <h3 class="m-portlet__head-text">
+                  Capital Expenditure Information
+                </h3>
               </div>
             </div>
           </div>
@@ -78,7 +99,6 @@
                     <label>Purpose*</label>
                   </b-col>
                   <b-col sm="8">
-                    {{purpose}}
                     <comp-select
                       :options="purposeData"
                       v-model="purpose"
@@ -89,7 +109,9 @@
                           : false
                       "
                     >
-                      <b-form-invalid-feedback id="purpose-feedback">Please select purpose.</b-form-invalid-feedback>
+                      <b-form-invalid-feedback id="purpose-feedback"
+                        >Please select purpose.</b-form-invalid-feedback
+                      >
                     </comp-select>
                   </b-col>
                 </b-row>
@@ -99,7 +121,6 @@
                     <label>Budget Approval Code*</label>
                   </b-col>
                   <b-col sm="8">
-                    {{budgetApprovalCode}}
                     <comp-select
                       v-if="!unbudget"
                       :options="budgetApprovalCodeData"
@@ -116,15 +137,18 @@
                       "
                       :disabled="unbudget"
                     >
-                      <b-form-invalid-feedback
-                        id="budget-appr-feedback"
-                      >Select Budget Approval Code Or Tick Unbudgetted.</b-form-invalid-feedback>
+                      <b-form-invalid-feedback id="budget-appr-feedback"
+                        >Select Budget Approval Code Or Tick
+                        Unbudgetted.</b-form-invalid-feedback
+                      >
                     </comp-select>
                   </b-col>
                 </b-row>
                 <b-row class="my-1">
                   <b-col sm="8" offset-sm="4">
-                    <b-form-checkbox v-model="unbudget">Unbudgetted</b-form-checkbox>
+                    <b-form-checkbox v-model="unbudget"
+                      >Unbudgetted</b-form-checkbox
+                    >
                   </b-col>
                 </b-row>
                 <b-row class="my-1">
@@ -154,7 +178,8 @@
                     <b-form-invalid-feedback
                       id="deskripsi-feedback"
                       v-if="!$v.description.required && $v.description.$error"
-                    >This field must not be empty.</b-form-invalid-feedback>
+                      >This field must not be empty.</b-form-invalid-feedback
+                    >
                   </b-col>
                 </b-row>
 
@@ -194,7 +219,8 @@
                         !$v.quantityText.requiredNumber &&
                           $v.quantityText.$error
                       "
-                    >This field must not be empty</b-form-invalid-feedback>
+                      >This field must not be empty</b-form-invalid-feedback
+                    >
                   </b-col>
                 </b-row>
 
@@ -214,7 +240,8 @@
                       <b-form-invalid-feedback
                         id="uom-feedback"
                         v-if="!$v.uom.required && $v.uom.$error"
-                      >Please select UoM.</b-form-invalid-feedback>
+                        >Please select UoM.</b-form-invalid-feedback
+                      >
                     </comp-select>
                     <!-- <b-form-select
                       size
@@ -244,7 +271,11 @@
                     <label>Delivery Date</label>
                   </b-col>
                   <b-col sm="8">
-                    <b-form-datepicker locale="id" type="date" v-model="deliveryDate"></b-form-datepicker>
+                    <b-form-datepicker
+                      locale="id"
+                      type="date"
+                      v-model="deliveryDate"
+                    ></b-form-datepicker>
                   </b-col>
                 </b-row>
 
@@ -279,7 +310,8 @@
                       v-if="
                         !$v.justification.required && $v.justification.$error
                       "
-                    >This field must not be empty.</b-form-invalid-feedback>
+                      >This field must not be empty.</b-form-invalid-feedback
+                    >
                   </b-col>
                 </b-row>
 
@@ -309,7 +341,8 @@
                           !$v.unitPriceText.requiredNumber &&
                             $v.unitPriceText.$error
                         "
-                      >This field must not be empty</b-form-invalid-feedback>
+                        >This field must not be empty</b-form-invalid-feedback
+                      >
                     </b-input-group>
                   </b-col>
                 </b-row>
@@ -320,7 +353,11 @@
                   </b-col>
                   <b-col sm="8">
                     <b-input-group size prepend="Rp">
-                      <b-form-input class="text-right" disabled :value="totalAmount | separator" />
+                      <b-form-input
+                        class="text-right"
+                        disabled
+                        :value="totalAmount | separator"
+                      />
                     </b-input-group>
                   </b-col>
                 </b-row>
@@ -373,7 +410,8 @@
                       <b-form-invalid-feedback
                         id="plant-feedback"
                         v-if="!$v.plant.required && $v.plant.$error"
-                      >Please select plant.</b-form-invalid-feedback>
+                        >Please select plant.</b-form-invalid-feedback
+                      >
                     </comp-select>
                     <!-- <b-form-select
                       size
@@ -416,7 +454,9 @@
                       <b-form-invalid-feedback
                         id="sloc-feedback"
                         v-if="!$v.storageLoc.required && $v.storageLoc.$error"
-                      >Please select storage location.</b-form-invalid-feedback>
+                        >Please select storage
+                        location.</b-form-invalid-feedback
+                      >
                     </comp-select>
                     <!-- <b-form-select
                       size
@@ -462,7 +502,10 @@
                     <label>Asset Type Activity</label>
                   </b-col>
                   <b-col sm="8">
-                    <comp-select :options="actTypeInfo" v-model="assetActivityType" />
+                    <comp-select
+                      :options="actTypeInfo"
+                      v-model="assetActivityType"
+                    />
                   </b-col>
                 </b-row>
 
@@ -471,7 +514,9 @@
                     <b-button variant="danger">Clear</b-button>
                   </b-col>
                   <b-col cols="5" class="text-left">
-                    <b-button variant="success" @click="validate">Submit</b-button>
+                    <b-button variant="success" @click="validate"
+                      >Submit</b-button
+                    >
                   </b-col>
                 </b-row>
               </b-container>
@@ -494,7 +539,7 @@ import { axiosCapex } from '../axios-instance';
 import { required, minLength, requiredIf } from 'vuelidate/lib/validators';
 import compSelect from './Select';
 
-const requiredNumber = (value) => {
+const requiredNumber = value => {
   if (value == '0') {
     return false;
   }
@@ -503,12 +548,12 @@ const requiredNumber = (value) => {
 
 export default {
   components: {
-    compSelect,
+    compSelect
   },
   filters: {
     separator(value) {
       return value.toLocaleString('ID');
-    },
+    }
   },
   data() {
     return {
@@ -547,7 +592,7 @@ export default {
       budgetCodeText: '',
       costCenter: '',
       budgetOwnerInfo: {},
-      totalBudget: 0,
+      totalBudget: 0
     };
   },
   computed: {
@@ -568,7 +613,7 @@ export default {
       }
 
       return this.unitPrice * this.quantity;
-    },
+    }
   },
   watch: {
     costCenter(newValue, oldValue) {
@@ -579,15 +624,15 @@ export default {
         this.budgetOwnerInfo = {};
       }
       this.budgetApprovalCodeData = this.budgetRaw
-        .filter((budget) => {
+        .filter(budget => {
           return budget.costCenter == newValue;
         })
-        .map((budget) => {
+        .map(budget => {
           return {
             ...budget,
             budgetDesc: `${budget.budgetCode} | ${budget.budgetDesc}`,
             name: `${budget.budgetCode} | ${budget.budgetDesc}`,
-            id: budget.budgetCode,
+            id: budget.budgetCode
           };
         });
       if (this.costCenter && !this.unbudget) {
@@ -604,54 +649,54 @@ export default {
     },
     budgetApprovalCode() {
       if (this.budgetApprovalCode) {
-        this.budgetOwnerInfo = this.budgetApprovalCodeData.find((value) => {
+        this.budgetOwnerInfo = this.budgetApprovalCodeData.find(value => {
           return value.budgetCode == this.budgetApprovalCode;
         });
         this.totalBudget = this.budgetOwnerInfo.budgetRemaining;
       }
-    },
+    }
   },
   validations: {
     costCenter: {
-      required,
+      required
     },
     purpose: {
-      required,
+      required
     },
     description: {
       required,
-      minLength: minLength(10),
+      minLength: minLength(10)
     },
     justification: {
       required,
-      minLength: minLength(10),
+      minLength: minLength(10)
     },
     // totalAmountText: {
     //   requiredNumber,
     // },
     unitPriceText: {
-      requiredNumber,
+      requiredNumber
     },
     quantityText: {
-      requiredNumber,
+      requiredNumber
     },
     uom: {
-      required,
+      required
     },
     plant: {
-      required,
+      required
     },
     storageLoc: {
-      required,
+      required
     },
     budgetApprovalCode: {
-      required: requiredIf(function (value) {
+      required: requiredIf(function(value) {
         if (this.unbudget && value.budgetApprovalCode == '') {
           return false;
         }
         return true;
-      }),
-    },
+      })
+    }
   },
   methods: {
     reset() {
@@ -735,7 +780,7 @@ export default {
               cancelTitle: 'NO',
               footerClass: 'p-2',
               hideHeaderClose: true,
-              centered: true,
+              centered: true
             }
           );
           if (result) {
@@ -750,7 +795,7 @@ export default {
             okVariant: 'danger',
             headerClass: 'p-2 border-bottom-0',
             footerClass: 'p-2 border-top-0',
-            centered: true,
+            centered: true
           });
           this.overlay = false;
         }
@@ -775,7 +820,7 @@ export default {
           plant: this.plant,
           storageLocation: this.storageLoc,
           deliveryDate: this.deliveryDate ? this.deliveryDate : '0000-00-00',
-          assetActivityType: this.assetActivityType,
+          assetActivityType: this.assetActivityType
         });
         this.$root.$bvToast.toast(`Capex ${result.data.ID} created`, {
           variant: 'primary',
@@ -783,7 +828,7 @@ export default {
           bodyClass: 'sm_toast__body ',
           noCloseButton: true,
           toaster: 'b-toaster-bottom-center',
-          autoHideDelay: 3000,
+          autoHideDelay: 3000
         });
         this.$router.push('/capex/' + result.data.ID);
       } catch (err) {
@@ -794,54 +839,54 @@ export default {
           okVariant: 'danger',
           headerClass: 'p-2 border-bottom-0',
           footerClass: 'p-2 border-top-0',
-          centered: true,
+          centered: true
         });
         this.overlay = false;
         this.errorMessage = err.response.data.message;
         this.submitText = 'Submit';
       }
-    },
+    }
   },
   created() {
     axiosCapex
       .get('/createInfo')
-      .then((result) => {
-        this.purposeData = result.data.purposeInfo.map((purpose) => {
+      .then(result => {
+        this.purposeData = result.data.purposeInfo.map(purpose => {
           return {
             id: purpose.purposeID,
-            name: purpose.purposeDesc,
+            name: purpose.purposeDesc
           };
         });
         this.budgetRaw = result.data.budgetInfo;
-        this.costCenterData = result.data.costCenterInfo.map((cc) => ({
+        this.costCenterData = result.data.costCenterInfo.map(cc => ({
           id: cc.costCenterCode,
-          name: `${cc.costCenterCode} | ${cc.costCenterName}`,
+          name: `${cc.costCenterCode} | ${cc.costCenterName}`
         }));
-        this.plantData = result.data.plantInfo.map((plant) => {
+        this.plantData = result.data.plantInfo.map(plant => {
           return { id: plant.plantCode, name: plant.plantName };
         });
-        this.storageLocData = result.data.slocInfo.map((sloc) => {
+        this.storageLocData = result.data.slocInfo.map(sloc => {
           return { id: sloc.slocCode, name: sloc.slocName };
         });
-        this.uomData = result.data.uomInfo.map((uom) => {
+        this.uomData = result.data.uomInfo.map(uom => {
           return {
             id: uom.uom,
-            name: uom.desc,
+            name: uom.desc
           };
         });
-        this.actTypeInfo = result.data.actTypeInfo.map((actType) => {
+        this.actTypeInfo = result.data.actTypeInfo.map(actType => {
           return {
             id: actType.actTypeCode,
-            name: actType.actTypeDesc,
+            name: actType.actTypeDesc
           };
         });
 
         this.actTypeInfo.unshift({ id: null, name: '' });
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
-  },
+  }
 };
 </script>
 
